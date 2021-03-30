5486929e6a0ecc0ed4a879a88780406dead127c9
Michael Klanica <michael.david.klanica@gmail.com>
2021-03-29 20:35:08 -0500

    CustomerController created, endpoint to get all customers

---
+++ src/main/java/com/michaelklanica/springmvcrest/controllers/CustomerController.java
@@ -0,0 +1,28 @@
+package com.michaelklanica.springmvcrest.controllers;
+
+import com.michaelklanica.springmvcrest.domain.Customer;
+import com.michaelklanica.springmvcrest.services.CustomerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(CustomerController.BASE_URL)
+public class CustomerController {
+
+    public static final String BASE_URL = "/api/v1/customers";
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    List<Customer> getAllCustomers(){
+        return customerService.findAllCustomers();
+    }
+
+}
